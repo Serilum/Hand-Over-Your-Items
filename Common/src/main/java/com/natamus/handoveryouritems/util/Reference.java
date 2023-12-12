fed913c2f67cc047082b83a4ae38933ef49682fd
Rick South <rick@natamus.com>
2023-12-12 09:59:34 +0100

    Hand Over Your Items source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/handoveryouritems/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.handoveryouritems.util;
+
+public class Reference {
+	public static final String MOD_ID = "handoveryouritems";
+	public static final String NAME = "Hand Over Your Items";
+	public static final String VERSION = "3.0";
+	public static final String ACCEPTED_VERSIONS = "[1.19.4]";
+}
