eb1ab5aa0ae34c4be2df26a03fa7b17fc6b6ea14
Rick South <rick@natamus.com>
2023-12-12 09:59:39 +0100

    Hand Over Your Items source code for 1.20.0.

---
+++ Fabric/src/main/java/com/natamus/handoveryouritems/ModFabric.java
@@ -0,0 +1,30 @@
+package com.natamus.handoveryouritems;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.handoveryouritems.events.HandOverEvent;
+import com.natamus.handoveryouritems.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+			return HandOverEvent.onPlayerClick(player, world, hand, entity, hitResult);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
