380c726d493b28a27cf852551deacf9c1ffdc673
Rick South <rick@natamus.com>
2024-05-26 14:22:50 +0200

    (v3.4) (1.20.6 Neo) Updated mod to work with NeoForge version 20.6.78+.

--- NeoForge/src/main/java/com/natamus/handoveryouritems/neoforge/events/NeoForgeHandOverEvent.java
+++ NeoForge/src/main/java/com/natamus/handoveryouritems/neoforge/events/NeoForgeHandOverEvent.java
@@ -2,10 +2,9 @@ package com.natamus.handoveryouritems.neoforge.events;
 
 import com.natamus.handoveryouritems.events.HandOverEvent;
 import net.minecraft.world.InteractionResult;
-import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
-import net.neoforged.bus.api.Event.Result;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 
 @EventBusSubscriber
 public class NeoForgeHandOverEvent {
@@ -13,7 +12,7 @@ public class NeoForgeHandOverEvent {
 	public static void onPlayerClick(PlayerInteractEvent.EntityInteract e) {
 		if (HandOverEvent.onPlayerClick(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null).equals(InteractionResult.FAIL)) {
 			e.setCanceled(true);
-			e.setResult(Result.DENY);
+			e.setCancellationResult(InteractionResult.FAIL);
 		}
 	}
 }
