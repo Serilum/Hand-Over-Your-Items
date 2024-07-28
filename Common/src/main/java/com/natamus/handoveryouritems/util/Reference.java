14d766c634740abe0e6091a04296ca353fce8c5f
Rick South <rick@natamus.com>
2024-07-28 07:29:58 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/handoveryouritems/util/Reference.java
+++ Common/src/main/java/com/natamus/handoveryouritems/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.handoveryouritems.util;
 public class Reference {
 	public static final String MOD_ID = "handoveryouritems";
 	public static final String NAME = "Hand Over Your Items";
-	public static final String VERSION = "3.4";
+	public static final String VERSION = "3.5";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
--- Forge/src/main/java/com/natamus/handoveryouritems/ModForge.java
+++ Forge/src/main/java/com/natamus/handoveryouritems/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.handoveryouritems;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.handoveryouritems.forge.config.IntegrateForgeConfig;
 import com.natamus.handoveryouritems.forge.events.ForgeHandOverEvent;
 import com.natamus.handoveryouritems.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
