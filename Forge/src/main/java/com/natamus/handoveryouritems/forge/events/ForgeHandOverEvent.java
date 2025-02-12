6def8c389b9a7a4e1ef980432633d4b5fbcaf338
Rick South <rick@natamus.com>
2025-02-12 23:30:34 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/handoveryouritems/forge/events/ForgeHandOverEvent.java
+++ Forge/src/main/java/com/natamus/handoveryouritems/forge/events/ForgeHandOverEvent.java
@@ -5,12 +5,10 @@ import net.minecraft.world.InteractionResult;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.Event.Result;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeHandOverEvent {
 	@SubscribeEvent
-	public void onPlayerClick(PlayerInteractEvent.EntityInteract e) {
+	public static void onPlayerClick(PlayerInteractEvent.EntityInteract e) {
 		if (HandOverEvent.onPlayerClick(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null).equals(InteractionResult.FAIL)) {
 			e.setCanceled(true);
 			e.setResult(Result.DENY);
--- NeoForge/src/main/java/com/natamus/handoveryouritems/neoforge/events/NeoForgeHandOverEvent.java
+++ NeoForge/src/main/java/com/natamus/handoveryouritems/neoforge/events/NeoForgeHandOverEvent.java
@@ -3,10 +3,8 @@ package com.natamus.handoveryouritems.neoforge.events;
 import com.natamus.handoveryouritems.events.HandOverEvent;
 import net.minecraft.world.InteractionResult;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 
-@EventBusSubscriber
 public class NeoForgeHandOverEvent {
 	@SubscribeEvent
 	public static void onPlayerClick(PlayerInteractEvent.EntityInteract e) {
