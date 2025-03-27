8d7718898b5920ce3910eeab948edbc3b2da672e
Rick South <rick@natamus.com>
2025-03-28 00:46:28 +0100

    Hand Over Your Items source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/handoveryouritems/forge/events/ForgeHandOverEvent.java
@@ -0,0 +1,17 @@
+package com.natamus.handoveryouritems.forge.events;
+
+import com.natamus.handoveryouritems.events.HandOverEvent;
+import net.minecraft.world.InteractionResult;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.Event.Result;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeHandOverEvent {
+	@SubscribeEvent
+	public static void onPlayerClick(PlayerInteractEvent.EntityInteract e) {
+		if (HandOverEvent.onPlayerClick(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null).equals(InteractionResult.FAIL)) {
+			e.setCanceled(true);
+			e.setResult(Result.DENY);
+		}
+	}
+}
