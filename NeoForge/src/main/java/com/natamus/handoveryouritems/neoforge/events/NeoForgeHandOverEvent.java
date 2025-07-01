640d1f1901fb802d5dc927e59e7b040c0ab49034
Rick South <rick@natamus.com>
2025-07-01 10:14:16 +0200

    Hand Over Your Items source code for 1.21.7.

---
+++ NeoForge/src/main/java/com/natamus/handoveryouritems/neoforge/events/NeoForgeHandOverEvent.java
@@ -0,0 +1,16 @@
+package com.natamus.handoveryouritems.neoforge.events;
+
+import com.natamus.handoveryouritems.events.HandOverEvent;
+import net.minecraft.world.InteractionResult;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+
+public class NeoForgeHandOverEvent {
+	@SubscribeEvent
+	public static void onPlayerClick(PlayerInteractEvent.EntityInteract e) {
+		if (HandOverEvent.onPlayerClick(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null).equals(InteractionResult.FAIL)) {
+			e.setCanceled(true);
+			e.setCancellationResult(InteractionResult.FAIL);
+		}
+	}
+}
