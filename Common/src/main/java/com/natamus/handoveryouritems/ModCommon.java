d6c476979af9edf30fed0be2e6a718f0fa5ae4a3
Rick South <rick@natamus.com>
2025-12-11 02:46:18 +0100

    Hand Over Your Items source code for 1.21.11.

---
+++ Common/src/main/java/com/natamus/handoveryouritems/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.handoveryouritems;
+
+import com.natamus.handoveryouritems.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
