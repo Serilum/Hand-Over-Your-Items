3e737e1818e1bb0e0425c7ba018fa31062861357
Rick South <rick@natamus.com>
2026-06-18 23:48:35 +0200

    Updated Hand Over Your Items to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/handoveryouritems/util/Reference.java
+++ Common/src/main/java/com/natamus/handoveryouritems/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "handoveryouritems";
 	public static final String NAME = "Hand Over Your Items";
 	public static final String VERSION = "3.8";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
